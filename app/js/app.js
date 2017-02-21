c4080621ea6798374c6164663059142fcd99e760
rugead <rugead@gmail.com>
2017-02-21 11:36:30 +0100

    more eslint and some codework on the simon game

--- app/js/app.js
+++ app/js/app.js
@@ -1,73 +1,90 @@
-let timeoutIDIgnore;
 const buzzerList = ['red', 'green', 'yellow', 'blue'];
-const app = document.querySelector('#app');
-// let randomMusic = Math.floor(Math.random() * 4);
+const buttons = document.querySelector('#buttons');
+const controls = document.querySelector('#controls');
+const playSingleSound = (id) => document.getElementById(id).play();
+const audioArray = ['redaudio', 'blueaudio', 'greenaudio', 'yellowaudio'];
+
 const createBuzzerIgnore = buzzerList.map(function (item) {
   let buzzer = document.createElement('DIV');
   buzzer.setAttribute('id', item);
   buzzer.style.backgroundColor = item;
   buzzer.style.borderColor = item;
   buzzer.classList.add(item, 'buzzer');
-  app.appendChild(buzzer);
+  buttons.appendChild(buzzer);
 });
 
 const startButton = document.createElement('DIV');
 startButton.setAttribute('id', 'start-button');
 startButton.innerHTML = 'start';
 startButton.classList.add('start-button');
-startButton.addEventListener('click', function() {
-  return playSound();
-}, false);
+startButton.addEventListener('click', () =>  picRandomSound(), false);
 
-app.appendChild(startButton);
+const resetButton = document.createElement('DIV');
+resetButton.setAttribute('id', 'reset-button');
+resetButton.innerHTML = 'reset';
+resetButton.classList.add('reset-button');
+resetButton.addEventListener('click', () => reset(), false);
 
 const runRandomSoundArray = document.createElement('DIV');
-runRandomSoundArray.setAttribute('id', 'runRandomSoundArray');
+runRandomSoundArray.setAttribute('id', 'run-button');
 runRandomSoundArray.innerHTML = 'run';
 runRandomSoundArray.classList.add('run-button');
-app.appendChild(runRandomSoundArray);
+runRandomSoundArray.addEventListener('click', function () { playRandomSoundArray(); }, false);
 
+controls.appendChild(resetButton);
+controls.appendChild(runRandomSoundArray);
+
+document.querySelector('#red').addEventListener('click', () => hhh('redaudio'));
+document.querySelector('#blue').addEventListener('click', () => hhh('blueaudio'));
+document.querySelector('#green').addEventListener('click', () => hhh('greenaudio'));
+document.querySelector('#yellow').addEventListener('click', () => hhh('yellowaudio'));
+
+let timeoutIDIgnore;
 let randomSoundArray = [];
+let playedSoundArray = [];
+let randomSoundArrayCounter = 0;
 
-document.querySelector('#red').addEventListener('click', function () { return play_single_sound('redaudio'); });
+let hhh = (sound) => {
+  playedSoundArray.push(sound);
+  let uuu = playedSoundArray.length - 1;
+  let ppp = randomSoundArray.length - 1;
+  if (playedSoundArray.indexOf(sound, uuu) === randomSoundArray.indexOf(sound, uuu) && (uuu === ppp)) {
+    playSingleSound(sound);
 
-document.querySelector('#blue').addEventListener('click', function () { return play_single_sound('blueaudio'); });
-
-document.querySelector('#green').addEventListener('click', function () { return play_single_sound('greenaudio'); });
-
-document.querySelector('#yellow').addEventListener('click', function () { return play_single_sound('yellowaudio'); });
-
-function play_single_sound(id) {
-  document.getElementById(id).play();
-}
-
-let audioArray = ['redaudio', 'blueaudio', 'greenaudio', 'yellowaudio'];
-
-let randomSoundArrayLength = () => {
-  var x = randomSoundArray.length;
-  playRandomSoundArray(x);
-};
-
-let counterRandomSound = 0;
-let counter = 0;
-let playSound = function () {
-  if (counterRandomSound === 0) {
-    let r = Math.floor(Math.random() * 4);
-    let s = audioArray[r];
-    console.log('RANDOMMUSIC', r);
-    randomSoundArray.push(s);
-    console.log(randomSoundArray);
-    document.getElementById(s).play();
+    setTimeout( () => playRandomSoundArray(), 1000);
+  } else if (playedSoundArray.indexOf(sound, uuu) === randomSoundArray.indexOf(sound, uuu)) {
+    playSingleSound(sound);
+  } else {
+    console.log('wrong should have been: ',  randomSoundArray[uuu]);
+    console.log('playedSoundArray: ', playedSoundArray[uuu]);
   }
 };
 
-let playRandomSoundArray = (x) => {
-  if (x < 0 ) { return; }
-  let y = randomSoundArray[0];
-  console.log('Y', y);
-  play_single_sound(y);
-  x = x - 1;
-  timeoutIDIgnore = setTimeout(function () { playRandomSoundArray(x); }, 1000);
+let picRandomSound = function () {
+  let r = Math.floor(Math.random() * 4);
+  let s = audioArray[r];
+  randomSoundArray.push(s);
+  playSingleSound(s);
 };
 
-runRandomSoundArray.addEventListener('click', function () { randomSoundArrayLength(); }, false);
+let playRandomSoundArray = (randomSoundArrayLength = (randomSoundArray.length)) => {
+  if (randomSoundArrayLength < 1 ) {
+    document.querySelector('#run-button').classList.add('disable');
+    picRandomSound();
+    playedSoundArray = [];
+    randomSoundArrayCounter = 0;
+    console.log(randomSoundArray);
+    return;
+  }
+  playSingleSound(randomSoundArray[randomSoundArrayCounter]);
+  randomSoundArrayCounter = randomSoundArrayCounter + 1;
+  randomSoundArrayLength = randomSoundArrayLength - 1;
+  timeoutIDIgnore = setTimeout(function () { playRandomSoundArray(randomSoundArrayLength); }, 1000);
+};
+
+let reset = () => {
+  document.querySelector('#run-button').classList.remove('disable');
+  randomSoundArray = [];
+  randomSoundArrayCounter = 0;
+  playedSoundArray = [];
+};
--- gulpfile.js
+++ gulpfile.js
@@ -12,80 +12,73 @@ var ghPages = require('gulp-gh-pages');
 const autoprefixer = require('gulp-autoprefixer');
 
 function bundle(bundler) {
-    return bundler
-      .bundle()
-      .on('error', function (e) {
-        gulputil.log(e.message);
+  return bundler
+    .bundle()
+    .on('error', function (e) {
+      gulputil.log(e.message);
     })
-      .pipe(exorcist('./build/js/dist/app.js.map'))
-      .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./build/js/dist'))
-      .pipe(browserSync.stream());
+    .pipe(exorcist('./build/js/dist/app.js.map'))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./build/js/dist'))
+    .pipe(browserSync.stream());
 }
 
 const srcPaths = {
-    pug: './app/*.pug',
-    css: './app/css/*.css'
+  pug: './app/*.pug',
+  css: './app/css/*.css'
 };
 
 const buildPaths = {
-    build: './build',
-    pug: './build/'
+  build: './build',
+  pug:  './build/'
 };
 
 gulp.task('pug', () => {
-    gulp.src(srcPaths.pug)
-      .pipe(pug())
-      .pipe(gulp.dest(buildPaths.pug));
+  return  gulp.src(srcPaths.pug)
+  .pipe(pug())
+  .pipe(gulp.dest(buildPaths.pug));
 });
 
 gulp.task('js', function () {
-    return bundle(browserify('./app/js/app.js'));
+  return  bundle(browserify('./app/js/app.js'));
 });
 
 gulp.task('css', function () {
-    return gulp.src(srcPaths.css)
+  return gulp.src(srcPaths.css)
         // .pipe(sourcemaps.init())
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
     }))
         // .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(srcPaths.pug, ['pug']);
-    gulp.watch(srcPaths.css, ['css']);
-
-    watchify.args.debug = true;
-    var watcher = watchify(browserify('./app/js/app.js', watchify.args).transform(babelify, { presets: ['es2015'] }));
-
+  gulp.watch(srcPaths.pug, ['pug']);
+  gulp.watch(srcPaths.css, ['css']);
+  watchify.args.debug = true;
+  var watcher = watchify(browserify('./app/js/app.js', watchify.args).transform(babelify, { presets: ['es2015'] }));
+  bundle(watcher);
+  watcher.on('update', function () {
     bundle(watcher);
+  });
 
-    watcher.on('update', function () {
-        bundle(watcher);
-    });
+  watcher.on( 'log' , gulputil.log );
+  var files = [ buildPaths.build ];
 
-    watcher.on('log', gulputil.log);
-
-    var files = [
-          buildPaths.build
-    ];
-
-    browserSync.init(files, {
-        browser: 'chrome',
-        server: './build',
-        logFileChange: false,
-        port: 8000
-    });
-
+  browserSync.init(files, {
+    browser: 'chrome',
+    server: './build',
+    logFileChange: false,
+    port: 8000
+  });
 });
 
 gulp.task('gh', function () {
-    return gulp.src('./build/**/*')
-      .pipe(ghPages());
+  return gulp.src('./build/**/*')
+ .pipe(ghPages());
 });
 
 gulp.task('deploy', ['pug', 'js', 'css', 'gh']);
